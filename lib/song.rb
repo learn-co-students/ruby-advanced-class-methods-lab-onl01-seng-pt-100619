049145757df74c68b4236201e96c4bf5d3968360
Alex Martinez <alexandr3martin3z@gmail.com>
2019-10-31 22:54:08 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +12,38 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+
+  end
+
+  def self.new_by_name
+    #def initialize(name)
+      #@name = name
+    Song.new
+
+  end
+
+  def self.create_by_name
+  end
+
+  def self.find_by_name
+  end
+
+  def self.find_or_create_by_name
+  end
+
+  def self.alphabetical
+
+  end
+
+  def self.new_from_filename #look up gsub
+  end
+
+  def self.create_from_filename
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+#binding.pry
 end
