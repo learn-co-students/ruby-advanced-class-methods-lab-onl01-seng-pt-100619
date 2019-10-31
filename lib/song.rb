eed8d951401225a40df5627a8f9d2493d1e16ec4
allison cortez <allizetorc@gmail.com>
2019-10-31 23:02:21 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,22 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    #initializes a song
+    song = self.new
+    #saves song to the @@all class variable...
+    # @@all << song
+    song.save
+    
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new 
+    song.name = name 
+    song 
+  end
+
 
 end
